6782017352efc284b59ae0c514d57c5df1ddb8ef
vrajmevada <81295552+vrajmevada@users.noreply.github.com>
2026-06-08 14:43:53 +0530

    purchase order controller

---
+++ ERPSystem.API/Controllers/PurchaseOrdersController.cs
@@ -0,0 +1,46 @@
+﻿using ERPSystem.Application.Features.Purchasing.DTOs;
+using ERPSystem.Application.Features.Purchasing.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ERPSystem.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class PurchaseOrdersController : ControllerBase
+{
+    private readonly IPurchaseOrderService _service;
+
+    public PurchaseOrdersController(
+        IPurchaseOrderService service)
+    {
+        _service = service;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<PurchaseOrderDto>>>
+        GetAll()
+    {
+        return Ok(await _service.GetAllAsync());
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<PurchaseOrderDto>>
+        GetById(int id)
+    {
+        var order = await _service.GetByIdAsync(id);
+
+        if (order == null)
+            return NotFound();
+
+        return Ok(order);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<PurchaseOrderDto>>
+        Create(CreatePurchaseOrderDto dto)
+    {
+        var result = await _service.CreateAsync(dto);
+
+        return Ok(result);
+    }
+}
